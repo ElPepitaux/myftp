b2d6ca2da2c955b00b129664a3c7255e7dd95d20
Nathan Maillot <nathan.maillot@epitech.eu>
2025-11-28 14:03:00 +0900

    fix: adjust behavior of USER in server

--- sources/server/commands/UserHandler.cpp
+++ sources/server/commands/UserHandler.cpp
@@ -20,6 +20,5 @@ void UserHandler::handle(const std::vector<std::string>& request, Connection& co
     if (request[1] != "anonymous") {
         throw ExceptionHandler("Only anonymous users are allowed.", 530);
     }
-    connection.setAuthenticated(true);
-    connection.write("230 User logged in, proceed.\r\n");
+    connection.write("331 Anonymous login ok, send your complete email as password.\r\n");
 }
